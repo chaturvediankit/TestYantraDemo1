3928bee5e27e6c81e6da367995b3a75455dc0252
chaturvediankit <chaturvediankit1993@gmail.com>
2024-06-04 03:37:54 +0000

    Base class changes

--- TestYantraDemo1/CustomMethods/BaseClass.cs
+++ TestYantraDemo1/CustomMethods/BaseClass.cs
@@ -1,13 +1,91 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestYantraDemo1.CustomMethods
 {
-    internal class BaseClass
+    public class BaseClass
     {
+        //This will launch browser in desired configuration
+        public static IWebDriver CreateWebDriver(string browserName)
+        {
+            switch (browserName.ToLower())
+            {
+                case "firefox":
+                    return CreateFirefoxDriver();
+
+                case "chrome":
+                    return CreateChromeDriver();
+                case "edge":
+                        return CreateEdgeDriver();
+                default:
+                    throw new ArgumentException("Invalid browser name");
+
+            }
+        }
+
+        private static IWebDriver CreateEdgeDriver()
+        {
+            IWebDriver driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+            return driver;
+
+        }
+
+        private static IWebDriver CreateChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            return new ChromeDriver(options);
+
+        }
+
+        private static IWebDriver CreateFirefoxDriver()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--start-maximized");
+            return new FirefoxDriver(options);
+
+        }
+
+        //Close Browser Instance
+
+        public static void CloseBrowser(IWebDriver driver)
+        {
+            try
+            {
+                driver.Quit();
+               /* Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
+                foreach(var process in chromeDriverProcesses)
+                {
+                    process.Kill();
+                }*/
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Issue encountered while closing browser");
+            }
+        }
+
+        //Custome method for entering a text in input field
+        public void EnterText(IWebElement element, string value)
+        {
+            element.Click();
+            element.Clear();
+            element.SendKeys(value);
+        }
+
+
+
 
     }
 }
